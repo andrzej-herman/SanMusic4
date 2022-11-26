d9a661bd1553e78995a36cd37cd1923e8dee9e38
andrzej-herman <aherman@san.edu.pl>
2022-11-26 18:25:06 +0100

    Start building backend

--- SanMusicApp/MusicLogic/IMusicService.cs
+++ SanMusicApp/MusicLogic/IMusicService.cs
@@ -1,6 +1,14 @@
-﻿namespace SanMusicApp.MusicLogic
+﻿using SanMusicCommon;
+
+namespace SanMusicApp.MusicLogic
 {
     public interface IMusicService
     {
+        IEnumerable<ShortSong> GetRandomSongs();
+        IEnumerable<ShortSong> SearchByTitleOrArtist(string pharase);
+        IEnumerable<ShortSong> GetSongsByCategory(string category);
+        IEnumerable<ShortSong> GetSongsLength(int min, int max);
+        IEnumerable<string> GetCategories();
+        Song GetSongById(string id);
     }
 }
